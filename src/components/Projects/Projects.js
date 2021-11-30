3882d9ff5af7283e1002c3e5e8b94681ccdde3c4
chandan-mandi <itscoderchandan@gmail.com>
2021-11-30 22:14:22 +0530

    title added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -62,7 +62,47 @@ const Projects = () => {
                                 <Card.Body>
                                     <Card.Title>Events-Maker - Event Manage.</Card.Title>
                                     <Card.Text>
-                                    determined to make your special day a memorable one. You can book your Event online by choosing it according to your budget.
+                                        determined to make your special day a memorable one. You can book your Event online by choosing it according to your budget.
+                                    </Card.Text>
+                                    <div className="links">
+                                        <ul>
+                                            <li >
+                                                <a href="https://events-maker.web.app/" target="_blank" rel="noreferrer">
+                                                    <FontAwesomeIcon icon={faExternalLinkAlt}></FontAwesomeIcon>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="https://github.com/chandan-mandi/events-maker" target="_blank" rel="noreferrer">
+                                                    <FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div className="project-tools">
+                                        <ul>
+                                            <li>React.js</li>
+                                            <li>React Bootstrap</li>
+                                            <li>Node.js</li>
+                                            <li>Express.js</li>
+                                            <li>MongoDB</li>
+                                            <li>Firebase Authentication</li>
+                                        </ul>
+                                    </div>
+                                </Card.Body>
+                            </Card>
+                        </div>
+                    </Col>
+                    <Col lg={4} md={4} sm={12} xs={12}>
+                        <div className="project-card">
+                            <Card>
+                                <div className="project-img p-2">
+                                    <Card.Img variant="top" src="https://raw.githubusercontent.com/chandan-mandi/chandanmandi-portfolio/main/src/assets/eventsmaker.png" />
+                                </div>
+                                <Card.Body>
+                                    <Card.Title>Holidify - Hotel booking engine.</Card.Title>
+                                    <Card.Text>
+                                        Holidify is India’s travel website, offering you the best prices on hotels
+                                        and holiday packages across India and the world.
                                     </Card.Text>
                                     <div className="links">
                                         <ul>
